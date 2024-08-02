cbede47b6e2732f488aeef0015f693ef50701549
Ahmad Jabbeer Jeerooburkhan <jabbeer@googlemail.com>
2024-08-02 11:10:00 +0000

    base.html template done

--- expense/views.py
+++ expense/views.py
@@ -3,13 +3,13 @@ from django.views.generic import TemplateView
 from django.views import generic
 from .models import Expense
 
-# class HomePage(TemplateView):
-#     """
-#     Displays home page"
-#     """
-#     template_name = 'index.html'
+class HomePage(TemplateView):
+    """
+    Displays home page"
+    """
+    template_name = 'base.html'
 
-class HomePage(generic.ListView):
-    queryset = Expense.objects.all()
-    template_name = "expense_list.html"
+# class HomePage(generic.ListView):
+#     queryset = Expense.objects.all()
+#     template_name = "expense_list.html"
     
