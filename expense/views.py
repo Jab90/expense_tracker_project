7b801acd17a032739af3cd2a70d1141221dab61d
Ahmad Jabbeer Jeerooburkhan <jabbeer@googlemail.com>
2024-08-02 19:02:04 +0000

    Added about me page

--- expense/views.py
+++ expense/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.views.generic import TemplateView
 from django.views import generic
 from .models import Expense
@@ -8,6 +8,3 @@ class HomePage(TemplateView):
     Displays home page"
     """
     template_name = "expense/index.html"
-
-
-    
--- expense_tracker/settings.py
+++ expense_tracker/settings.py
@@ -29,7 +29,7 @@ TEMPLATES_DIR = os.path.join(BASE_DIR, 'templates')
 SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ["8000-jab90-expensetrackerpr-rgjprr4zh02.ws.codeinstitute-ide.net", '.herokuapp.com']
 
@@ -47,6 +47,7 @@ INSTALLED_APPS = [
     'cloudinary_storage',
     'cloudinary',
     'expense',
+    'about',
 ]
 
 CSRF_TRUSTED_ORIGINS = [
